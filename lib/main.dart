872982e2053451a5c796536dfdfc77b497de33d7
urayusuke <crfnf690@yahoo.co.jp>
2020-10-28 22:37:32 +0900

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(new MyApp());
@@ -27,85 +28,169 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _message;
-
-  @override
-  void initState() {
-    _message = 'ok.';
-    super.initState();
-  }
+  String _message = 'ok';
+  bool _switch = true;
+  double _slider = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 235, 235, 235),
       appBar: AppBar(
         title: Text('App Name'),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              _message,
-              style: TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w400,
-                  fontFamily: "Roboto"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-            ),
-            RaisedButton(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            _message,
+            style: TextStyle(
+                fontSize: 32.0,
+                color: const Color(0xFF000000),
+                fontWeight: FontWeight.w400,
+                fontFamily: "Roboto"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+          ),
+
+          //スイッチ
+          CupertinoSwitch(
+            value: _switch,
+            onChanged: (bool value) {
+              print('switch');
+              setState(() {
+                _switch = value;
+                _message = 'switch: $_switch';
+              });
+            },
+          ),
+
+          //スライダー
+          CupertinoSlider(
+            value: _slider,
+            min: 0.0,
+            max: 1.0,
+            divisions: 100,
+            onChanged: (double value) {
+              print(value);
+              setState(() {
+                _slider = value;
+                _message = 'slider: $_slider';
+              });
+            },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: CupertinoButton(
+              borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+              pressedOpacity: 0.5,
+              color: CupertinoColors.activeBlue,
               onPressed: buttonPressed,
-              padding: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(20.0),
               child: Text(
                 "tap me!",
                 style: TextStyle(
-                    fontSize: 28.0,
-                    color: const Color(0xFF000000),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-              ),
-            ),
-          ]),
-    );
-  }
-
-  void buttonPressed() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) => Column(
-        children: <Widget>[
-          Text('This is Modal Bottom Sheet!',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w300,
-                color: Colors.black,
-              )),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-          ),
-          FlatButton(
-            onPressed: () => Navigator.pop<String>(context, 'Close'),
-            child: const Text(
-              'Close',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.cyan,
+                  fontSize: 28.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto",
+                ),
               ),
             ),
           ),
         ],
       ),
+
+      //ナビゲーションバー
+      bottomNavigationBar: CupertinoNavigationBar(
+        leading: Icon(CupertinoIcons.left_chevron),
+        middle: Text('Navigation'),
+        trailing: IconButton(
+          icon: Icon(CupertinoIcons.right_chevron),
+          onPressed: showPicker,
+        ),
+      ),
+    );
+  }
+
+  Widget buttonPressed() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text("Hello!"),
+        actions: <Widget>[
+          CupertinoDialogAction(
+              child: const Text('Cancel'),
+              onPressed: () => Navigator.pop<String>(context, 'Cancel')),
+          CupertinoDialogAction(
+              child: const Text('OK'),
+              onPressed: () => Navigator.pop<String>(context, 'OK'))
+        ],
+      ),
     ).then<void>((value) => resultAlert(value));
   }
 
+  //ダイアログアクション・イベント
   void resultAlert(String value) {
     setState(() {
       _message = 'selected: $value';
     });
+  }
+
+  //ピッカー表示
+  void showPicker() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoPicker(
+          backgroundColor: CupertinoColors.black,
+          itemExtent: 50.0,
+          children: <Widget>[
+            Text(
+              'One',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            Text(
+              'Two',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            Text(
+              'Three',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            Text(
+              'Four',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+            Text(
+              'Five',
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+            ),
+          ],
+          onSelectedItemChanged: (int value) {
+            print('pick $value');
+          },
+        );
+      },
+    );
   }
 }
