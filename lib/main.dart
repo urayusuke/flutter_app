5026676198c213256ce7406d0203abf39949cd5d
urayusuke <crfnf690@yahoo.co.jp>
2020-10-30 16:41:10 +0900

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -57,58 +57,32 @@ class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
 
 class _MyRenderBox extends RenderBox {
   ui.Image _img;
+  Offset _pos;
 
   @override
   bool hitTest(HitTestResult result, {@required Offset position}) {
+    result.add(BoxHitTestEntry(this, position));
     return true;
   }
 
-  _MyRenderBox() {
-    loadAssetImage('image.jpg');
+  @override
+  void handleEvent(PointerEvent event, HitTestEntry entry) {
+    super.handleEvent(event, entry);
+    _pos = event.position;
+    markNeedsPaint();
   }
-
-  loadAssetImage(String fname) => rootBundle.load("assets/$fname").then((bd) {
-        Uint8List u8lst = Uint8List.view(bd.buffer);
-        ui.instantiateImageCodec(u8lst).then((codec) {
-          codec.getNextFrame().then((frameInfo) {
-            _img = frameInfo.image;
-            markNeedsPaint();
-            print("_img created: $_img");
-          });
-        });
-      });
 
   @override
   void paint(PaintingContext context, Offset offset) {
     Canvas c = context.canvas;
-    int dx = offset.dx.toInt();
-    int dy = offset.dy.toInt();
-
-    Path path = Path();
-    Rect r = Rect.fromLTWH(dx + 50.0, dy + 50.0, 75.0, 75.0);
-    path.addOval(r);
-    r = Rect.fromLTWH(dx + 75.0, dy + 75.0, 125.0, 125.0);
-    path.addOval(r);
-    r = Rect.fromLTWH(dx + 125.0, dy + 125.0, 175.0, 175.0);
-    path.addOval(r);
-
-    c.save();
-
-    Paint p = Paint();
-    p.color = Color.fromARGB(150, 255, 0, 0);
-    p.style = PaintingStyle.fill;
-    c.drawPath(path, p);
-
-    c.translate(0.0, 100.0);
-    p.color = Color.fromARGB(150, 0, 0, 255);
-    c.drawPath(path, p);
-
-    p.color = Color.fromARGB(150, 0, 255, 0);
-    c.rotate(-0.5 * pi);
-    c.translate(-600.0, -200.0);
-    c.scale(1 * 1.75);
-    c.drawPath(path, p);
-
-    c.restore();
+    c.drawColor(Colors.black, BlendMode.clear);
+    if (_pos != null) {
+      Paint p = Paint();
+      p.style = PaintingStyle.fill;
+      for (var i = 0; i < 10; i++) {
+        p.color = Color.fromARGB(50, 255, 255, 255);
+        c.drawCircle(_pos, i * 5.0, p);
+      }
+    }
   }
 }
